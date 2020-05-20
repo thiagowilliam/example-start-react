abd414e6fb881e423fe11a613d6a10b9ead85146
thiago william <thiago.will.silva@gmail.com>
2020-05-20 15:54:22 +0100

    template project

---
+++ src/pages/Example/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styles';
+
+export default function example() {
+  return (
+    <S.Container>
+      <h1>Page Example</h1>;
+    </S.Container>
+  );
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styles';
+
+export default function Home() {
+  return (
+    <S.Container>
+      <h1>Home</h1>;
+    </S.Container>
+  );
+}
